c84ba61799e2a450b00df98c32dc435da3a7662d
Ebrahim Al Aghbari <83554966+E-Aghbari@users.noreply.github.com>
2023-05-28 01:52:12 +0100

    Added Player class and its attribute and methods for now

---
+++ PalaceExchanger.py
@@ -0,0 +1,23 @@
+class Player:
+    def __init__(self, name, cards):
+        self.name = name
+        self.cards = cards
+        self.spells = []
+        self.health = 20
+        self.armor = 0
+    
+    def choose_cards(self):
+        pass
+
+    def draw_card(self):
+        pass
+
+    def gain_armor(self):
+        pass
+
+    def gain_health(self):
+        pass
+
+    def __repr__(self):
+        pass
+
