35375364ed24ea71a270973fd062f2a17bc532b6
Ebrahim Al Aghbari <Alaghbarie@cardiff.ac.uk>
2023-05-28 18:31:37 +0100

    added warriors and other things

--- PalaceExchanger.py
+++ PalaceExchanger.py
@@ -1,44 +1,67 @@
 class Player:
-    def __init__(self, name, cards):
+    def __init__(self, name):
         self.name = name
-        self.cards = cards
+        self.cards = []
         self.spells = []
         self.health = 20
         self.armor = 0
+        self.knocked_out = False
     
-    def choose_cards(self):
-        pass
+    def choose_cards(self, card):
+        card = input("Choose a card: ")
+        if card not in self.cards:
+            self.cards.append(card)
+        else:
+            print("You already have that card.")
 
-    def draw_card(self):
-        pass
+    def choose_spells(self, spell):
+        spell = input("Choose a spell: ")
+        if spell not in self.spells:
+            self.spells.append(spell)
 
     def gain_armor(self):
-        pass
+        self.armor += 5
 
     def gain_health(self):
+        if self.knocked_out:
+            print("You can't gain health while knocked out.")
+        else:
+            self.health += 5
+
+    def lose_armor(self):
         pass
 
-    def use_potion(self):
+    def lose_health(self):
         pass
 
     def __repr__(self):
-        pass
+        return f"{self.name} has {self.health} health and {self.armor} armor. His cards are {self.cards} and his spells are {self.spells}."
 
 class Card:
-    def __init__(self, name, type, health):
-        pass
+    warriors = {"Knight": [2, 2], "Paladin": [3, 1], "Berserker": [1, 3], "Archer": [2, 2], "Assassin": [1, 3], "Wizard": [3, 1]}
+    def __init__(self, name, level = 1):
+        if name in Card.warriors.keys():
+            self.name = [name]
+            self.level = level
+            self.health = Card.warriors[name][1] + level
+            self.damage = Card.warriors[name][0] + level
 
-    def attack(self):
+
+    def attack(self, other_card):
         pass
 
     def use_potion(self):
         pass
 
     def __repr__(self):
-        pass
+        return f"This level {self.level} warrior named {self.name} has {self.damage} worth of damage and {self.health} HP"
 
 class Spell:
     spells = ["Holy", "Natural", "Fire", "Frost"]
 
     def __repr__(self):
         pass
+
+
+
+print(Card("Paladin", 3))
