143036b1fca60bb59df494ff7a28f371237690c2
Ebrahim Al Aghbari <83554966+E-Aghbari@users.noreply.github.com>
2023-05-29 01:38:46 +0100

    Worked on it

--- PalaceExchanger.py
+++ PalaceExchanger.py
@@ -1,8 +1,8 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, cards, spells = 0):
         self.name = name
-        self.cards = []
-        self.spells = []
+        self.cards = cards
+        self.spells = spells
         self.health = 20
         self.armor = 0
         self.knocked_out = False
@@ -28,27 +28,47 @@ class Player:
         else:
             self.health += 5
 
-    def lose_armor(self):
-        pass
+    def lose_armor(self, amount):
+        self.armor -= amount
 
-    def lose_health(self):
-        pass
+    def lose_health(self, amount):
+        self.health -= amount
+        if self.health <= 0:
+            self.knocked_out = True
 
     def __repr__(self):
         return f"{self.name} has {self.health} health and {self.armor} armor. His cards are {self.cards} and his spells are {self.spells}."
 
 class Card:
     warriors = {"Knight": [2, 2], "Paladin": [3, 1], "Berserker": [1, 3], "Archer": [2, 2], "Assassin": [1, 3], "Wizard": [3, 1]}
+    
     def __init__(self, name, level = 1):
         if name in Card.warriors.keys():
             self.name = name
             self.level = level
             self.health = Card.warriors[name][1] + level
             self.damage = Card.warriors[name][0] + level
+            self.player = None
+
+    def assign_player(self, player_):
+        if type(player_) is Player:
+            self.player = (player_)
 
 
     def attack(self, other_card):
-        pass
+        if other_card.player.armor > 0:
+            other_card.player.lose_armor(self.damage)
+        else:    
+            other_card.health -= self.damage
+
+        if other_card.health == 0:
+            self.player.cards.remove(other_card)
+            print(f"{other_card.name} got defeated.")
+        if self.damage > other_card.health:
+            if self.player.knocked_out:
+                print(f"{self.player.name} got knocked out!")
+                return
+            other_card.player.lose_health(self.damage - other_card.health)
 
     def use_potion(self):
         pass
@@ -60,8 +80,29 @@ class Spell:
     spells = ["Holy", "Natural", "Fire", "Frost"]
 
     def __repr__(self):
-        pass
+        return f"This spell does this and thatz"
+
+player1 = Player("Ebrahim")
+player2 = Player("Abdullah")
+
+print(f"Before :{player1}")
+print(f"Before :{player2}")
+print()
 
 
+card1 = Card("Knight")
+card2 = Card("Archer", 9)
+card1.assign_player(player1)
+card2.assign_player(player2)
 
-print(Card("Paladin", 3))
+card1.assign_cards()
+card2.assign_cards()
+
+
+card2.attack(card1)
+print(card1.player.name)
+print(card2.player.name)
+print()
+
+print(player1)
+print(player2)
