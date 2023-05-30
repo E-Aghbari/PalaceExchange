b887c1c66d9cc45dc514054c7b1c4aec1c8390ef
Ebrahim Al Aghbari <Alaghbarie@cardiff.ac.uk>
2023-05-30 20:05:40 +0100

    Game is finished but needs a lot of clarification and modification

--- PalaceExchanger.py
+++ PalaceExchanger.py
@@ -1,16 +1,16 @@
-warriors = {"Knight": [2, 2], "Paladin": [3, 1], "Berserker": [1, 3], "Archer": [2, 2], "Assassin": [1, 3], "Wizard": [3, 1]}
+
 
 class Player:
     def __init__(self, name ):
         self.name = name
         self.cards = []
-        self.health = 20
+        self.health = 5
         self.armor = 0
         self.knocked_out = False
     
     def choose_cards(self):
         while len(self.cards) != 3:
-            card = input(f"Choose cards: {warriors}\n")
+            card = input(f"Choose cards: {Card.warriors}\n")
             if card not in self.cards:
                 card = Card(card, player = self)
                 self.cards.append(card)
@@ -36,7 +36,7 @@ class Player:
             self.knocked_out = True
 
     def __repr__(self):
-        return f"{self.name} has {self.health} health and {self.armor} armor. His cards are {self.cards}."
+        return f"{self.name} has {self.health} health and {self.armor} armor. His cards are {[ elem.name for elem in self.cards]}.\n"
 
 class Card:
     warriors = {"Knight": [2, 2], "Paladin": [3, 1], "Berserker": [1, 3], "Archer": [2, 2], "Assassin": [1, 3], "Wizard": [3, 1]}
@@ -51,6 +51,7 @@ class Card:
                 self.player = player
             else:
                 print("This hero does not exist.")
+                
             
 
     def removed(self):
@@ -62,23 +63,22 @@ class Card:
 
     def attack(self, other_card):
         if self.damage > other_card.health:
+            if other_card.player.armor > 0:
+                other_card.player.lose_armor(self.damage - other_card.health)
+            else:
+                other_card.player.lose_health(self.damage - other_card.health)  
             other_card.health = 0
             other_card.player.cards.remove(other_card)
-            print(f"{other_card.name} got defeated.")
+            print(f"{other_card.name} got defeated.")          
         else:
             other_card.health -= self.damage
 
-        if other_card.player.armor > 0:
-            other_card.player.lose_armor(self.damage - other_card.health)
-        else:
-            other_card.player.lose_health(self.damage - other_card.health)
-
         if other_card.player.knocked_out:
-            print(f"{self.player.name} got knocked out!")
+            print(f"{other_card.player.name} got knocked out!")
             
             
     def __repr__(self):
-        return f"This level {self.level} warrior named {self.name} has {self.damage} worth of damage and {self.health} HP"
+        return f"This level {self.level} warrior named {self.name} has {self.damage} worth of damage and {self.health} HP.\n"
 
 
 class Game:
@@ -104,24 +104,58 @@ class Game:
 
 
 current_game =  Game()
-player1 = Player("Ebrahim")
-player2 = Player("Abdullah")
+player1 = Player("John")
+player2 = Player("Drake")
 
 
 
 players = [player1, player2]
 
 
+print("\nWelcome to Palace Exchanger!\n")
+
+print("Player 1, choose 3 cards.")
+for i in range(3):
+    player1.choose_cards()
+
+print("\nPlayer 2, choose your cards.")
+for i in range(3):
+    player2.choose_cards()
+
+print(player1, player2)
+
+input("Press Enter to start the game.\n")
 
 while not current_game.gameOver:
-    for player in players:
-            player.choose_cards()    
+    print(f"{len(player1.cards)}, and {len(player2.cards)}")   
     current_game.newRound()
-    card1 = int(input(f"{player1.name} choose a card from your deck to attack\nYour Deck:{[card.name for card in player1.cards]}\n"))
-    card2 = int(input(f"{player2.name} choose a card from your deck to attack\nYour Deck:{[card.name for card in player2.cards]}\n"))
-    player1.cards[card1].attack(player2.cards[card2]) 
-    player2.cards[card2].attack(player1.cards[card1])
+    
+    try:   
+        card1 = int(input(f"{player1.name} choose the index number of the card from your deck to attack\n\nYour Deck:{[card.name for card in player1.cards]}\n"))
+        card2 = int(input(f"{player2.name} choose the index number of the card from your deck to attack\n\nYour Deck:{[card.name for card in player2.cards]}\n"))
+    except:
+        print("You must enter a number.")
+        continue
+
+    try:
+        player1.cards[card1].attack(player2.cards[card2]) 
+        player2.cards[card2].attack(player1.cards[card1])
+        print(player1.cards[card1], player2.cards[card2]) 
+    except:
+        if len(player1.cards) != 3:
+            print("Player 1, choose 3 cardss.")
+            for i in range(3):
+                player1.choose_cards()
+                
+
+        if len(player2.cards) != 3:
+            print("\nPlayer 2, choose your cards.")
+            for i in range(3):
+                player2.choose_cards() 
     print(player1, player2)
+    print(player1.cards, player2.cards) 
+
     current_game.checkWin(player1, player2)
+
     
 
